27c92b5c8aa11bef1b62a75106df17e5f55e35b2
John Terenzio <john@terenz.io>
2021-12-23 10:17:38 -0600

    Disable some gpg stuff for Kubuntu

--- conf.d/gpg-agent.fish
+++ conf.d/gpg-agent.fish
@@ -2,7 +2,8 @@ status is-interactive; or exit
 type -q gpg-agent; or exit
 not set -q SSH_TTY; or exit
 
-set -gx GPG_TTY (tty)
+# Commened out lines for systems without GUI pin entry
+#set -gx GPG_TTY (tty)
 set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 
 function gpg-set-tty
@@ -11,19 +12,19 @@ function gpg-set-tty
     set -U fish_gpg_tty $GPG_TTY
   end
 end
-gpg-set-tty
+#gpg-set-tty
 
-function git
-  gpg-set-tty
-  command git $argv
-end
+#function git
+  #gpg-set-tty
+  #command git $argv
+#end
 
-function gpg
-  gpg-set-tty
-  command gpg $argv
-end
+#function gpg
+  #gpg-set-tty
+  #command gpg $argv
+#end
 
-function ssh
-  gpg-set-tty
-  command ssh $argv
-end
+#function ssh
+  #gpg-set-tty
+  #command ssh $argv
+#end
