b5021e28179bc952d0ebd6293a1eb3a80689af8f
John Terenzio <john@terenz.io>
2022-01-04 17:30:39 -0600

    fzf open

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -5,3 +5,12 @@ set -gx FZF_DEFAULT_OPTS "--history=/tmp/fzfhist"
 if type -q rg
   set -gx FZF_DEFAULT_COMMAND "rg --files"
 end
+
+function open
+  set -l file (fzf)
+  if string match -r -i ASCII (file $file)
+    vim $file
+  else
+    xdg-open $file &> /dev/null
+  end
+end
