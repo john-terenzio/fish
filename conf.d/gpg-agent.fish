6af49edf06f8e756ddf29358c89301c8ef6db6a5
John Terenzio <john@terenz.io>
2025-08-04 20:15:46 -0500

    Update gpg scripts for MacOS

--- conf.d/gpg-agent.fish
+++ conf.d/gpg-agent.fish
@@ -2,29 +2,9 @@ status is-interactive; or exit
 type -q gpg-agent; or exit
 not set -q SSH_TTY; or exit
 
-# Commened out lines for systems without GUI pin entry
-#set -gx GPG_TTY (tty)
+set -gx GPG_TTY (tty)
 set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 
-function gpg-set-tty
-  if test "$fish_gpg_tty" != "$GPG_TTY"
-    gpg-connect-agent updatestartuptty /bye > /dev/null
-    set -U fish_gpg_tty $GPG_TTY
-  end
+if not pgrep gpg-agent > /dev/null
+  gpg-connect-agent updatestartuptty /bye &> /dev/null
 end
-#gpg-set-tty
-
-#function git
-  #gpg-set-tty
-  #command git $argv
-#end
-
-#function gpg
-  #gpg-set-tty
-  #command gpg $argv
-#end
-
-#function ssh
-  #gpg-set-tty
-  #command ssh $argv
-#end
