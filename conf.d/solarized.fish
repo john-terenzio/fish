f7dc08936749fb2bb1da6a40ba7a692a271a3810
John Terenzio <john@terenz.io>
2025-12-31 14:54:02 -0600

    Update theme to use global variables

--- conf.d/solarized.fish
+++ conf.d/solarized.fish
@@ -17,29 +17,29 @@ set -l red     red
 set -l violet  brmagenta
 set -l yellow  yellow
 
-set -U fish_color_autosuggestion $base01
-set -U fish_color_cancel -r
-set -U fish_color_command $base1
-set -U fish_color_comment $base01
-set -U fish_color_cwd $green
-set -U fish_color_cwd_root $red
-set -U fish_color_end $cyan
-set -U fish_color_error $red
-set -U fish_color_escape $blue
-set -U fish_color_history_current --bold
-set -U fish_color_host $base1
-set -U fish_color_host_remote $yellow
-set -U fish_color_match --background=$base02
-set -U fish_color_normal $base1
-set -U fish_color_operator $blue
-set -U fish_color_param $base0
-set -U fish_color_quote $base00
-set -U fish_color_redirection $violet
-set -U fish_color_search_match $base00 --background=$base02
-set -U fish_color_selection $base2 --background=$base03
-set -U fish_color_user $base01
-set -U fish_color_valid_path --underline
-set -U fish_pager_color_completion $base1
-set -U fish_pager_color_description $base1
-set -U fish_pager_color_prefix $cyan --underline
-set -U fish_pager_color_progress $base3 --background=$cyan
+set --global fish_color_autosuggestion $base01
+set --global fish_color_cancel -r
+set --global fish_color_command $base1
+set --global fish_color_comment $base01
+set --global fish_color_cwd $green
+set --global fish_color_cwd_root $red
+set --global fish_color_end $cyan
+set --global fish_color_error $red
+set --global fish_color_escape $blue
+set --global fish_color_history_current --bold
+set --global fish_color_host $base1
+set --global fish_color_host_remote $yellow
+set --global fish_color_match --background=$base02
+set --global fish_color_normal $base1
+set --global fish_color_operator $blue
+set --global fish_color_param $base0
+set --global fish_color_quote $base00
+set --global fish_color_redirection $violet
+set --global fish_color_search_match $base00 --background=$base02
+set --global fish_color_selection $base2 --background=$base03
+set --global fish_color_user $base01
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_completion $base1
+set --global fish_pager_color_description $base1
+set --global fish_pager_color_prefix $cyan --underline
+set --global fish_pager_color_progress $base3 --background=$cyan
